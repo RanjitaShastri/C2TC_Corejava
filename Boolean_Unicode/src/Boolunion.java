f3f0202c62f03da2157d6194634fa8a5da9a6992
Ranjita Shastri <Ranjita Shastri@DESKTOP-HH5QA0I>
2022-10-11 15:25:12 +0530

    First commit

---
+++ Boolean_Unicode/src/Boolunion.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ranjita Shastri
+ *
+ */
+public class Boolunion {
+
+	
+	public static void main(String[] args) {
+		
+		boolean var = false;
+		System.out.println(var);
+		char var1 = '\u0047';
+		System.out.println(var1);
+
+	}
+
+}
