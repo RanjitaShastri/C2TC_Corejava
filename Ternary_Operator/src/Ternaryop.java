f85e2f59fab599b88f097b6628601f99699ac6cc
Ranjita Shastri <Ranjita Shastri@DESKTOP-HH5QA0I>
2022-10-11 15:42:48 +0530

    First commit

---
+++ Ternary_Operator/src/Ternaryop.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ranjita Shastri
+ *
+ */
+public class Ternaryop {
+
+	
+	public static void main(String[] args) {
+		
+		
+		int x = 10;;
+		x = (10==x) ? 1:0;
+		System.out.println(x);
+
+	}
+
+}
