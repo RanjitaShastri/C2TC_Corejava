841bfe15f77bdecbcafff20f7601765d629d1b58
Ranjita Shastri <Ranjita Shastri@DESKTOP-HH5QA0I>
2022-10-11 07:35:53 +0530

    First commit

---
+++ C2TC_Project1/src/FirstProgram.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ranjita Shastri
+ *
+ */
+public class FirstProgram {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello World!");
+		
+
+	}
+
+}
