f2ceaa8ca933f9c9b472e14c2429f21c492c0658
Ranjita Shastri <Ranjita Shastri@DESKTOP-HH5QA0I>
2022-10-11 15:59:40 +0530

    First commit

---
+++ Assignment_Operator/src/Assignment.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ranjita Shastri
+ *
+ */
+public class Assignment {
+   
+	
+	public static void main(String[] args) {
+
+
+		int x = 4;
+	    x+=5;      // x=x+5;
+	//x-=5, x=x-5;
+	//x*=5, x=x*5;
+	//x/=5, x=x/5;
+	//x%=5, x=x%5;
+	System.out.println(x);
+
+
+	}
+
+}
