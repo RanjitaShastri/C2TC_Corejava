a3dfbba90c87522d24247477c66d40aa6b11d3cf
Ranjita Shastri <Ranjita Shastri@DESKTOP-HH5QA0I>
2022-10-11 15:30:26 +0530

    First commit

---
+++ TypeCasting/src/typecast.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ranjita Shastri
+ *
+ */
+public class typecast {
+
+	
+	public static void main(String[] args) {
+		
+		float f = 10.532f;
+		long l = (long)f;
+		System.out.println(l);
+
+	}
+
+}
