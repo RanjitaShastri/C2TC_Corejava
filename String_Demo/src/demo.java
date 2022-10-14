10e99b99c49c82dcea478248d41b46e7f5cc4bce
Ranjita Shastri <Ranjita Shastri@DESKTOP-HH5QA0I>
2022-10-14 13:30:59 +0530

    First commit

---
+++ String_Demo/src/demo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ranjita Shastri
+ *
+ */
+public class demo {
+
+	
+	public static void main(String[] args) {
+
+
+		String var = new String("Hello world");
+		System.out.println(var);
+
+	}
+}
