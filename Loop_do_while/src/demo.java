8fed9cb0075734f4f8e78e1e4f1c719f09973cb6
Ranjita Shastri <Ranjita Shastri@DESKTOP-HH5QA0I>
2022-10-14 14:07:26 +0530

    First commit

---
+++ Loop_do_while/src/demo.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ranjita Shastri
+ *
+ */
+public class demo {
+
+	
+	public static void main(String[] args) {
+		
+		int i = 11;
+		do
+		{
+			System.out.println(i);
+			i++;	
+		}
+		
+		while(i<=1);
+	}
+}
+
+
+	
+
+
