3ae861cbc83c9afd8a0ee618f4f520c342395d92
Ranjita Shastri <Ranjita Shastri@DESKTOP-HH5QA0I>
2022-10-14 13:52:42 +0530

    First commit

---
+++ If_Else/src/hello.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ranjita Shastri
+ *
+ */
+public class hello {
+
+	
+	public static void main(String[] args) {
+		
+		int x = 4;	
+		
+		  if (x > 5) 
+		  {
+		  	System.out.println("X is greater than 5"); 
+		   }
+		  else 
+		  {
+		  System.out.println("X is smaller than 5"); 
+		  } 
+
+	}
+
+}
