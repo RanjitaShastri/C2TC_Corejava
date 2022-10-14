22868a7e7174e77b993ba65f7669443d49acd2ce
Ranjita Shastri <Ranjita Shastri@DESKTOP-HH5QA0I>
2022-10-14 14:18:03 +0530

    First commit

---
+++ Loops_Demo/src/demo.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ranjita Shastri
+ *
+ */
+public class demo {
+
+	
+	public static void main(String[] args) {
+		
+		    
+		  // for loop
+		
+		  for(int i=1,j=1; i<10||j<10;i++,j++)
+		  { 
+			  j++;
+			  System.out.println("i="+i+" and j="+j); 
+		  }
+
+
+		  // while loop 
+			  int m =1,n =1;
+			  while(m<10 || n<10)
+			  { 
+				  m++; n++;
+			      System.out.println("m="+ m +" and n="+n); 
+			  }	
+			
+		// Do while loop
+			int a = 1, b = 1;
+			do {
+				a++;
+				b++;
+				System.out.println("a=" + a + " and b =" + b);
+			    } while (a < 10 || b < 10);
+		}
+	}
+
+
+	
+
+
