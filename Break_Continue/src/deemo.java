a687c60d85cf968060406b644857bbd9c8141144
Ranjita Shastri <Ranjita Shastri@DESKTOP-HH5QA0I>
2022-10-14 14:24:23 +0530

    First commit

---
+++ Break_Continue/src/deemo.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ranjita Shastri
+ *
+ */
+public class deemo {
+
+	
+	public static void main(String[] args) {
+		
+        for(int i=1; i<=10; i++)
+        {
+       	 if(i == 5)
+       	 {
+       		continue;
+       		// break;       
+       	 }
+       	 System.out.println(i);
+        }
+
+
+	}
+
+}
